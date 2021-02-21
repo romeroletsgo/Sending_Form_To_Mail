c14a7eb0d34594aacc4f6e8cd2f2146ff5939394
romeroletsgo <roman_shkapov@mail.ru>
2021-02-21 13:51:44 +0300

    Добавление валидации JS, добавление файла на JS, отправки формы на JS, смена общего стиля страницы, добавление phpmailer,настройка phpmailer.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function formSend(e) {
         e.preventDefault();
+
         let error = formValidate(form);
+
+        let formData = new FormData(form);
+        formData.append('image', formImage.files[0]);
+
+        if (error === 0) {
+            form.classList.add('__sending');
+            //     let response = await fetch('sendmail.php', {
+            //         method: 'POST',
+            //         body: formData
+            //     });
+            //     if(response.ok){
+            //         let result = await response.json();
+            //         alert(result.message);
+            //         formPreview.innerHTML = '';
+            //         form.reset();
+            //         form.classList.remove('__sending');
+            //     } else {
+            //         alert('Ошибка');
+            //         form.classList.remove('__sending');
+            //     }
+            // } else {
+            //     alert('Заполните обязательные поля')
+            // }
+        }
     }
 
     function formValidate(form) {
@@ -32,6 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             }
         }
+        return error;
     }
 
     function formAddError(input) {
@@ -44,8 +70,41 @@ document.addEventListener('DOMContentLoaded', function () {
         input.classList.remove('__error');
     }
 
-     //Функция теста email регулярным выражением
+    //Функция теста email регулярным выражением
 	function emailTest(input) {
 		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
 	}
+
+    //Получаем input file в переменную
+    const formImage = document.getElementById('formImage');
+    //Получаем div для превью в переменную
+    const formPreview = document.getElementById('formPreview');
+
+    //Слушаем изменения в инпуте file
+    formImage.addEventListener('change', () => {
+        uploadFile(formImage.files[0]);
+    });
+
+    function uploadFile(file) {
+        //Проверяем тип файла
+        if (!['image/jpeg', 'image/png', 'image/gif', 'image/jpg'].includes(file.type)) {
+            alert('Разрешены только изображения.');
+            formImage.value = '';
+            return;
+        }
+        //Проверяем размер файла (<2 Мб)
+        if (file.size > 2 * 1024 * 1024) {
+            alert('Файл должен быть менее 2 МБ.');
+            return;
+        }
+
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+        };
+        reader.onerror = function (e) {
+            alert('Ошибка');
+        };
+        reader.readAsDataURL(file);
+    }
 });
